1c590c23134bb31c78e75a2d761bc408b6dc1185
harsh-javascript <harsh.patel@simformsolutions.com>
2022-05-12 11:05:46 +0530

    css class change

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -83,19 +83,19 @@ function Tweet({ tweet }: Props) {
       </div>
       <div className="mt-5 flex justify-between">
         <div
-          className="items-cente flex cursor-pointer space-x-3 text-gray-400"
+          className="flex cursor-pointer items-center space-x-3 text-gray-400"
           onClick={() => session && setCommentBoxVisible(!commentBoxVisible)}
         >
           <ChatAlt2Icon className="h-5 w-5" />
           <p>{comments.length}</p>
         </div>
-        <div className="items-cente flex cursor-pointer space-x-3 text-gray-400">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <SwitchHorizontalIcon className="h-5 w-5" />
         </div>
-        <div className="items-cente flex cursor-pointer space-x-3 text-gray-400">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <HeartIcon className="h-5 w-5" />
         </div>
-        <div className="items-cente flex cursor-pointer space-x-3 text-gray-400">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
           <UploadIcon className="h-5 w-5" />
         </div>
       </div>
