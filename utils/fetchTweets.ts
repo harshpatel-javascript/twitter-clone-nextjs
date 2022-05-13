0480070e820105fea011f3a4a9df3901577bcfbd
harsh-javascript <harsh.patel@simformsolutions.com>
2022-05-13 12:31:16 +0530

    absolute url added

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,9 +1,9 @@
-import { Tweet } from '../typing'
+import { Tweet } from '../typing';
 
 export const fetchTweets = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`)
-  const data = await res.json()
-  const tweets: Tweet[] = data.tweets
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
+  const data = await res.json();
+  const tweets: Tweet[] = data.tweets;
 
-  return tweets
-}
+  return tweets;
+};
