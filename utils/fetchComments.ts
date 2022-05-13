dd5b2e0aae7265a86e718a9a46656f56071c5dc0
harsh-javascript <harsh.patel@simformsolutions.com>
2022-05-13 12:28:18 +0530

    absolute url added

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,7 +1,9 @@
 import { Comment } from '../typing';
 
 export const fetchComments = async (tweetId: string) => {
-  const res = await fetch(`/api/getComments?tweetId=${tweetId}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`
+  );
 
   const comments: Comment[] = await res.json();
   return comments;
